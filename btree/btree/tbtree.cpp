f5ce7c0a77aa04e1f06f69945d7edb402ceadda8
glumpo <kaartemij@yandex.ru>
2018-05-03 08:48:08 +0300

    Destructor moved to cpp

--- btree/btree/tbtree.cpp
+++ btree/btree/tbtree.cpp
@@ -4,11 +4,21 @@ TBTree::TBTree() {
 
 }
 
+TBTree::~TBTree() {
+    if (root) {
+        /* Every item has a correct destructor,
+             * so it will delete every object in a tree
+             * recursively
+             * NOTE: May be its better to delete manualy, without recursion. */
+        for (int i = 0; i < root.Size(); ++i) {
+            delete root[i];
+        }
+    }
+}
+
 bool TBTree::Insert(TBTreeItem item) {
     if (!root) {
         root = new TBTreeNode();
-        return root.Push(item);
     }
-
 
 }
--- btree/btree/tbtree.h
+++ btree/btree/tbtree.h
@@ -3,23 +3,14 @@
 
 #include "tbtreeitem.h"
 #include "tbtreenode.h"
+#include "list.h"
 
 class TBTree
 {
 public:
     const auto T_OF_TREE = 2;
     TBTree();
-    ~TBTree() {
-        if (root) {
-            /* Every item has a correct destructor,
-             * so it will delete every object in a tree
-             * recursively
-             * NOTE: May be its better to delete manualy, without recursion. */
-            for (int i = 0; i < root.Size(); ++i) {
-                delete root[i];
-            }
-        }
-    }
+    ~TBTree();
 
     TBTreeItem::ValueType Search(TBTreeItem::KeyType k);
     TBTreeItem::ValueType    Pop(TBTreeItem::KeyType k);
